552adf80d10a6fda2369b570d0dc7b5bb916a084
Paulino <pnrjr@hotmail.com>
2026-02-18 09:52:15 -0300

    Configura Supabase e script de setup SQL

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: "https://nueeuujmqldnzfnpitnq.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51ZWV1dWptcWxkbnpmbnBpdG5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0MTM4NjEsImV4cCI6MjA4Njk4OTg2MX0.80Od5gr_GojJ1PE_T_4S5EITY4oukyN5zAPK2b5-rU4",
+};
